defb4bd259b35d1f4da1cacb94c9c84b99c11b61
Md Tanvir Alam <tanvirgeek@gmail.com>
2013-10-04 13:45:50 +0600

    hr

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 $(document).on('pop-initialized', function(){
     $(".asset-inner").each(function(){
         if($(this).children(".asset-title")){
-            alert('if');
+            alert($(this).children(".asset-title").html());
         }
         else{
             $(this).append('<hr class="cool-hr-tag">');
