d7e0381d6d23367360fcebb164594920c8662c3a
Md Tanvir Alam <tanvirgeek@gmail.com>
2013-10-04 17:55:54 +0600

    Final??

--- js/main.js
+++ js/main.js
@@ -2,15 +2,8 @@
 
 $(document).on('pop-initialized', function(){
     $(".asset-inner").each(function(){
-        
-        alert($(this).children(".asset-title").html());
-        
         if($(this).children(".asset-title").html() === undefined){
-              alert("what the hack");
               $(this).append('<hr class="cool-hr-tag">');
         }
-        else{
-            alert("not working");
-        }
-    });
+     });
 });
